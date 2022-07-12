4ec4b35d6d4a9f9863872f5c6dfb0a8bb30a9d17
HtB#1526 <93080546+HtB1526@users.noreply.github.com>
2022-07-12 08:12:55 +0300

    Update Kill R63 with DOOM music.lua

--- Scripts/Kill R63 with DOOM music.lua
+++ Scripts/Kill R63 with DOOM music.lua
@@ -3,12 +3,10 @@ By HtB#1526
 Published in t.me/arceusxscripts
 ]]
 local VeryBigHead = _G.VeryBigHead or false
-local V3
+local V3=Vector3.new(6.30,6.30,6.30)
 if VeryBigHead then 
     V3=Vector3.new(250,250,250) 
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-839.69091796875, 3.1843905448913574, 912.049560546875)
-else 
-    V3=Vector3.new(6.30,6.30,6.30)
 end
 for _, v in pairs(workspace.SlederSpawn:GetChildren())do
     for _,_v in pairs(v:GetChildren())do
