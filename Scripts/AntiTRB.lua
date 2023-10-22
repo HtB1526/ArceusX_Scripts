3119b5df127de649e7a959666c98003980b4f88b
HtB1526 <93080546+HtB1526@users.noreply.github.com>
2023-10-22 19:08:28 +0300

    AntiTRB.lua

---
+++ Scripts/AntiTRB.lua
@@ -0,0 +1,12 @@
+local cfg={DS={"i have child porn on my pc!","i watch child porn!","i enjoy child porn!","i make child porn!","i bomb a school!"
+,"i will rape children!","im a child pedophile!","i support pedophiles!","i groom children!","i plan a school bombing!","i will rape a child!"
+,"i support school bombers!","i rape children when bombing their schools!","niggers, we dont have a term msg rn. so we can just say: i rape children! i support terrorism!"},MI=workspace.lmao}
+pcall(function()
+    for _,v in pairs(getrenv()._G)do
+        if type(v)=="string"and v:lower():find(cfg.DS)then
+            game.Players.LocalPlayer:Kick"AntiTRB triggered, ur acc saved"
+            cfg.MI.Playing=true cfg.MI.Volume=10
+            wait(0.75)while true do end
+        end
+    end
+end)
