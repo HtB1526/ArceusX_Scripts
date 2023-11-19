a8d0d8f0a8b51c41bcd0a15b92d03c991701105f
HtB1526 <93080546+HtB1526@users.noreply.github.com>
2023-11-19 20:32:47 +0300

    Update vulnerabilitie patcher.lua
    updated script link, block all requests to roblox.com

--- Scripts/vulnerabilitie patcher.lua
+++ Scripts/vulnerabilitie patcher.lua
@@ -1,13 +1,21 @@
---//this will prevent a malicious script for load + anti-robux stealer
+--//this will prevent a malicious script for load + anti-robux stealer + block all requests to roblox
 --//<3 https://t.me/arceusxscripts
-local rl,ms=loadstring,game:HttpGet"https://raw.githubusercontent.com/h4xal0t/wtf/main/Protected_9909131513277395.lua.txt"
-getgenv().vp_msg=function(str,ts,wt)
+local ms=game:HttpGet"https://raw.githubusercontent.com/sekrte3345544/Roblox/main/new-obfuscated.lua"
+local vp={
+    ls=loadstring,
+    request=request,
+    http_request=http_request,
+    hr=http.request,
+    sr=syn.request,
+    fr=fluxus.request
+}
+getgenv().l0adstring=function(str,ts,wt)
     local a=Instance.new("Message",workspace)local b=string.split(str,"")for c=0,#b,1 do if b[c]~=nil then a.Text=a.Text..b[c]wait(ts)end end;wait(wt)b=string.split("https://t.me/arceusxscripts/5612","")a.Text=""for c=0,#b,1 do if b[c]~=nil then a.Text=a.Text..b[c]wait(ts)end end;wait(wt)a:Destroy()
 end
 wait()
 getgenv().loadstring=function(s)
-    if s==ms then return function()getgenv().vp_msg("Prevented a malicious script to load",0.05,2.5)end end
-    return rl(s)
+    if s==ms then return function()getgenv().l0adstring("Prevented a malicious script to load",0.05,2.5)end end
+    return vp.ls(s)
 end
 if getrawmetatable and setreadonly then
     --//as i think this better bc some exploits dev's cannot "getgenv().hookmetamethod=function(inst,nc,func)local rmt=getrawmetatable blah blah blah"
@@ -16,7 +24,7 @@ if getrawmetatable and setreadonly then
         local ar=({...})[2]
         if(ar=="PromptGamePassPurchase"or ar=="PromptBundlePurchase"or ar=="PromptProductPurchase"or ar=="PromptPurchase"or ar=="PromptSubscriptionPurchase")and (checkcaller and checkcaller()or true)then --//if no checkcaller, block all GamePassPurchase
             warn("Blocked "..tostring(ar).." that was created by your exploit // by https://t.me/arceusxscripts")
-            getgenv().vp_msg("Prevented a robux stealer script load",0.05,2.5)
+            getgenv().l0adstring("Prevented a robux stealer script load",0.05,2.5)
             return nil
         end
         return oi(...)
@@ -24,3 +32,19 @@ if getrawmetatable and setreadonly then
 else
     warn("your executor doesnt support getrawmetatable()or setreadonly(), so anti-robux stealer cannot load")
 end
+pcall(function()
+    local function cf(o,n)
+        getgenv()[o]=n
+    end
+    local function wrap(t,o)
+        if string.find(t.Url, "https://", 1, false)and string.find(t.Url, "roblox.com", 1, false)then
+            return{Success=true,StatusCode=200,StatusMessage="OK",Headers={},Cookies={},Body="{\"status\":\"OK\"}"}
+        end
+        return o(t)
+    end
+    if request~=nil then cf("request",function(t)return wrap(t,vp.request)end)end
+    if http_request~=nil then cf("http_request",function(t)return wrap(t,vp.http_request)end)end
+    if http~=nil and http.request~=nil then getgenv().http.request=function(t)return wrap(t,vp.hr)end end
+    if syn~=nil and syn.request~=nil then getgenv().syn.request=function(t)return wrap(t,vp.sr)end end
+    if fluxus~=nil and fluxus.request~=nil then getgenv().fluxus.request=function(t)return wrap(t,vp.fr)end end
+end)
